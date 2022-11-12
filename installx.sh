6b8d75f67c7d5c40d1dae97db0f4e7378bd4bacd
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-12 13:07:27 +0100

    Create installx.sh

---
+++ installx.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+sudo apt install -y icewm
+
+sudo apt install -y xinit
+
+sudo apt install -y x11-xserver-utils
+
+sudo apt install -y build-essential
+
+sudo apt install -y libx11-dev libsft-dev libharfbuzz-dev libxinerama-dev
+
+sudo apt install -y xterm gedit firefox pcmanfm nitrogen ubuntu-wallpapers-jammy 
+
+
