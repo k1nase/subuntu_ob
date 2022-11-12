359e5f1a8319aeab5c1f68196e0e7a9fb52cbfb4
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-12 13:33:53 +0100

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -1,14 +1,26 @@
 #!bin/bash
 
+B=$(tput bold)
+N=$(tput sgr0)
+
+
+echo "${B}##Installation icewm##${N}"
+
 sudo apt install icewm -y
+
+echo "${B}##Installation serveur X##${N}"
 
 sudo apt install xinit -y
 
-sudo apt install x11-xserver-utils -y
+echo "${B}##Installation utilitaires serveur X##${N}"
+
+sudo apt install x11-xserver-utils libx11-dev libsft-dev libharfbuzz-dev libxinerama-dev -y
+
+echo "${B}##Installation utilitaires de build X##${N}"
 
 sudo apt install build-essential -y
 
-sudo apt install libx11-dev libsft-dev libharfbuzz-dev libxinerama-dev -y
+echo "${B}##Installation des logiciels de base + wallapers##${N}"
 
 sudo apt install xterm gedit firefox pcmanfm nitrogen ubuntu-wallpapers-jammy -y
 
