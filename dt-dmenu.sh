e7993ef2c712d7eb1f5556d59418005fb338edc5
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-12 13:36:23 +0100

    Update dt-dmenu.sh

--- dt-dmenu.sh
+++ dt-dmenu.sh
@@ -1,5 +1,14 @@
+
+B=$(tput bold)
+N=$(tput sgr0)
+
+
+echo "${B}##Récupération et installation de la configuration dmenu de distrotube##${N}"
+
 sudo git clone https://gitlab.com/dwt1/dmenu-distrotube
 
 cd dmenu-distrotube
 
 sudo make install
+
+echo "${B}---Rappel : dmenu se lance avec 'dmenu_run'---${N}"
