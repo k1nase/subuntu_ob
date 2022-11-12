38a6e7ca6087f2a9a98b1497f30d16958dde1867
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-12 17:06:11 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -18,3 +18,7 @@ sudo update-rc.d -f nitrogen --restore defaults
 echo "${B}###Configuration autostart###${N}"
 cp ~/sh/autostart ~/.config/openbox
 chmod +x ~/.config/openbox/autostart
+
+echo "${B}###Configuration raccourcis###${N}"
+cp ~/sh/rc.xml ~/.config/openbox
+chmod +x ~/.config/openbox/autostart
