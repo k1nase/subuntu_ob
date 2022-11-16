55796a85f1b643cdfd91ab60a9ae320493942d8e
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-16 14:45:18 +0100

    Update 1-installx.sh

--- 1-installx.sh
+++ 1-installx.sh
@@ -16,12 +16,18 @@ echo "${B}##Installation utilitaires de build X##${N}"
 
 sudo apt install build-essential -y
 
-echo "${B}##Installation utilitaires de openbox##${N}"
+echo "${B}##Installation utilitaires de l'interface graphique##${N}"
 
-sudo apt install openbox -y
+sudo apt install openbox polybar sddm -y
 
 echo "${B}##Installation des logiciels de base + wallapers##${N}"
 
-sudo apt install xterm gedit firefox pcmanfm nitrogen ubuntu-wallpapers-jammy suckless-tools -y
+sudo apt install xterm gedit firefox pcmanfm nitrogen ubuntu-wallpapers-jammy suckless-tools xpdf numlockx -y
+
+echo "${B}##Installation de alacritty##${N}"
+
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo apt update
+sudo apt install alacritty -y
 
 echo "${B}--Vous pouvez maintenant tester le fonctionnement de l'interface graphique en tapant 'startx' ${N}"
