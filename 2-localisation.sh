5aa9264d9995ea78aa858882ef93dc72d71270aa
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-16 14:42:54 +0100

    Update and rename localisation.sh to 2-localisation.sh

--- 2-localisation.sh
+++ 2-localisation.sh
@@ -4,11 +4,7 @@ B=$(tput bold)
 N=$(tput sgr0)
 
 
-echo "${B}##Installation et configuration du gestionnaire de clavier##${N}"
 
-dpkg-reconfigure keyboard-configuration
-
-setxkbmap fr
 
 echo "${B}##Configuration heure de Paris##${N}"
 
@@ -17,6 +13,10 @@ sudo timedatectl set-ntp yes
 sudo timedatectl set-timezone Europe/Paris
 
 
+echo "${B}##Installation et configuration du gestionnaire de clavier##${N}"
 
+dpkg-reconfigure keyboard-configuration
+
+setxkbmap fr
 
 
